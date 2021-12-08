4ca3410b946017c695e5107d713cb2e745851bd1
Emilio <Edwardestevez95@gmail.com>
2021-12-07 22:17:40 -0600

    pushing mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,30 +1,6 @@
 const { gql } = require('apollo-server-express');
 
-// * `User` type:
 
-// * `_id`
-
-// * `username`
-
-// * `email`
-
-// * `bookCount`
-
-// * `savedBooks` (This will be an array of the `Book` type.)
-
-// * `Book` type:
-
-// * `bookId` (Not the `_id`, but the book's `id` value returned from Google's Book API.)
-
-// * `authors` (An array of strings, as there may be more than one author.)
-
-// * `description`
-
-// * `title`
-
-// * `image`
-
-// * `link`
 
 const typeDefs = gql`
   type User {
@@ -56,7 +32,7 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    saveBook(bookId: ID!, authors: [String]!, description: String!,title: String!, image: String!, link: String): User
+    saveBook(bookId: ID!, authors: [String]!, description: String!,title: String!, image: String!, link: String!): User
     removeBook(bookId: ID!): User
   }
 `;
